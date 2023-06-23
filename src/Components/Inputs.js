c3af99c318a9325f4b25b0388cc5d66e60dffc12
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-06-23 13:39:20 +1200

    drip working need formatting

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,26 +1,26 @@
 import React from 'react'
+import '../drip.scss'
+
+
+//https://codepen.io/jeffmccarthyesq/pen/vYVdRvN
+//https://create-react-app.dev/docs/adding-a-sass-stylesheet/
+
 
 const Inputs = (props) => {
 
     return (
         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 0 0" width="0" height="0" style="display:none;">
-                <defs>
-                    <filter id="goo">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
-                    </filter>
-                </defs>
-            </svg>
 
-            <div class="goo">
+
+
+            <div className="goo">
                 <h1>{Number(props.first)}   {props.operation}  {Number(props.second)}   {props.answer}</h1>
-                <div class="drop"></div>
-                <div class="drop"></div>
-                <div class="drop"></div>
-                <div class="drop"></div>
-                <div class="drop"></div>
-                <div class="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
             </div>
 
 
