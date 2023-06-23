a1bc783ebec19b8574abac5b35ddd80a0d59edcd
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-06-23 13:04:06 +1200

    update with scss

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -4,9 +4,26 @@ const Inputs = (props) => {
 
     return (
         <div>
-            <label>
-                {Number(props.first)}   {props.operation}  {Number(props.second)}   {props.answer}
-            </label>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 0 0" width="0" height="0" style="display:none;">
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                    </filter>
+                </defs>
+            </svg>
+
+            <div class="goo">
+                <h1>{Number(props.first)}   {props.operation}  {Number(props.second)}   {props.answer}</h1>
+                <div class="drop"></div>
+                <div class="drop"></div>
+                <div class="drop"></div>
+                <div class="drop"></div>
+                <div class="drop"></div>
+                <div class="drop"></div>
+            </div>
+
+
         </div>
     )
 }
